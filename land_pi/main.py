fbdacafaa13ae116318ec1822e46d53e6ae69c52
myrccar <4myrccar@gmail.com>
2022-09-06 14:21:13 -0700

    working on my pc

--- land_pi/main.py
+++ land_pi/main.py
@@ -1,13 +1,19 @@
 #script for land pi 
 #tested on my pc only
-import socket
+import socket,time
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((socket.gethostname(),6060))
 s.listen(5)
 
 while True:
+    runing = True
     clientSocket, address = s.accept()
     print(f"conctson made: {address}")
-    clientSocket.send(bytes("hi","utf-8"))
+    while runing:
+        message_out = input("enter message to send")
+        if message_out == "stop": runing = False
+        
+        clientSocket.send(bytes(message_out,"utf-8"))
+        time.sleep(0.1)
     clientSocket.close()
--- rov_pi/main.py
+++ rov_pi/main.py
@@ -6,6 +6,6 @@ import socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 6060))
 
-message = s.recv(2048)
-
-print(f"Message reseved: {message}")
+while True:
+    message = s.recv(2048)
+    print(f"Message reseved: {message}")
