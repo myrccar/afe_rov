63ee7e103d4404a549f8d7d4b94dee2dcc00e6ee
myrccar <4myrccar@gmail.com>
2022-09-06 14:03:34 -0700

    socket setup

--- land_pi/main.py
+++ land_pi/main.py
@@ -1,2 +1,8 @@
-#script for land pi
+#script for land pi 
+#tested on my pc only
+from pstats import SortKey
+import socket
 
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind(socket.gethostname(),6060)
+s.listen(5)
