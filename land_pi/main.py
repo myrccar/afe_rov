ea6c8c66aa0093f85bbb5b94cfe0584be331cd66
myrccar <4myrccar@gmail.com>
2022-09-06 14:13:11 -0700

    client

--- land_pi/main.py
+++ land_pi/main.py
@@ -3,7 +3,7 @@
 import socket
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(socket.gethostname(),6060)
+s.bind((socket.gethostname(),6060))
 s.listen(5)
 
 while True:
--- rov_pi/main.py
+++ rov_pi/main.py
@@ -1,1 +1,11 @@
 #script for rov pi
+#tested on my pc ONLY
+from email import message
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 6060))
+
+message = s.recv(2048)
+
+print(f"Message reseved: {message}")
