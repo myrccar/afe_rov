e68a59c28b29a3c140d5e633afb40bdf8bf659e0
myrccar <4myrccar@gmail.com>
2022-09-06 14:09:21 -0700

    connecting

--- land_pi/main.py
+++ land_pi/main.py
@@ -1,8 +1,13 @@
 #script for land pi 
 #tested on my pc only
-from pstats import SortKey
 import socket
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind(socket.gethostname(),6060)
 s.listen(5)
+
+while True:
+    clientSocket, address = s.accept()
+    print(f"conctson made: {address}")
+    clientSocket.send(bytes("hi","utf-8"))
+    clientSocket.close()
