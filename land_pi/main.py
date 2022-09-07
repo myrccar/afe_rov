4e835759063669877d20c1abdfa2efdb1c16c3a5
myrccar <4myrccar@gmail.com>
2022-09-06 17:01:53 -0700

    working with json

--- land_pi/main.py
+++ land_pi/main.py
@@ -1,19 +1,23 @@
 #script for land pi 
 #tested on my pc only
-import socket,time
+import socket,time,json,sys
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((socket.gethostname(),6060))
-s.listen(5)
+s.listen(1)
 
 while True:
     runing = True
     clientSocket, address = s.accept()
     print(f"conctson made: {address}")
-    while runing:
-        message_out = input("enter message to send")
-        if message_out == "stop": runing = False
-        
-        clientSocket.send(bytes(message_out,"utf-8"))
-        time.sleep(0.1)
-    clientSocket.close()
+
+    #when connected
+    while True:
+        #axis are -500/500, buttons are true/false
+        controller_data = {"x":426,"y":-234,"dpad-up":True}
+        controller_data = json.dumps(controller_data)
+        clientSocket.sendall(bytes(controller_data,"utf-8"))
+        #check if user wants to quit
+        if input("quit? ") == "yes": 
+            clientSocket.close()
+            sys.exit()
--- rov_pi/main.py
+++ rov_pi/main.py
@@ -1,11 +1,15 @@
 #script for rov pi
 #tested on my pc ONLY
 from email import message
-import socket
+import socket,json
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 6060))
 
 while True:
     message = s.recv(2048)
-    print(f"Message reseved: {message}")
+    controller_data = json.loads(str(message).strip("b'").strip("'"))
+    print("data resevied:")
+    print(f"x-axis: {controller_data['x']}")
+    print(f"y-axis: {controller_data['y']}")
+    print(f"d-pad up: {controller_data['dpad-up']}")
