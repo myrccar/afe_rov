961699bc5a38c36fac74b70db29f16674056eb4e
myrccar <4myrccar@gmail.com>
2022-10-08 18:23:35 -0700

    fixed land pi

--- land_pi/main.py
+++ land_pi/main.py
@@ -1,34 +1,48 @@
-#script for land pi 
-#tested on my pc only
+'''
+script for land pi 
+tested on my pc only
+
+this scirpt will connect to the rov_pi with socket
+then send over json data from controller_inputs.py
+
+json key: axis are -500/500, buttons are true/false
+
+by myrccar a.k.a sami j
+'''
 import socket
 import time
 import json
 import sys
 from custom_imports.controller_input import controller
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind((socket.gethostname(),6060))
-s.listen(1)
+def quit():
+    if input("quti?") == "yes":
+        return True
+    else: return False
 
-def test_client(client):
-    #not done yet
-    pass
+def socket_func():
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+        s.bind((socket.gethostname(),6060))
+        s.listen(1)
 
-controller = controller()
+        while True:
 
-while True:
-    runing = True
-    clientSocket, address = s.accept()
-    print(f"conctson made: {address}")
-    controller.read()
-    #when connected
-    while True:
-        controller.read()
-        #axis are -500/500, buttons are true/false
-        controller_data = {"x":426,"y":-234,"dpad-up":True}
-        controller_data = json.dumps(controller_data)
-        clientSocket.sendall(bytes(controller_data,"utf-8"))
-        #check if user wants to quit
-        if input("quit? ") == "yes": 
-            clientSocket.close()
-            sys.exit()
+            clientSocket, address = s.accept()
+            try:
+                with clientSocket as c:
+                    print(f"connected to {address}")
+                    while True:
+                        controller_data = {"x":426,"y":-234,"dpad-up":True}
+                        controller_data = json.dumps(controller_data)
+                        c.sendall(bytes(controller_data,"utf-8"))
+                        time.sleep(0.5)
+                        if quit():
+                            return
+            except:
+                print("client fail-ed")
+            if quit():
+                return
+
+socket_func()
+print("bye")
+
