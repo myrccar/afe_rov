fffdfd6fbabbba7ff36526482b3e20c0f9e6fea6
myrccar <4myrccar@gmail.com>
2023-01-16 17:21:15 -0800

    idk

--- rov_pi/main.py
+++ rov_pi/main.py
@@ -7,7 +7,7 @@ GPIO.setmode(GPIO.BOARD)             # choose BCM or ---BOARD(one we use)---
 GPIO.setup(12, GPIO.OUT)
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(('192.168.0.99', 6060))
+s.connect(('192.168.0.99', 3400))
 
 try:
     while True:
