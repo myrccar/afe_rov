7b3cd10a43a8442608857b5cce2f97509332b7ff
myrccar <4myrccar@gmail.com>
2023-01-16 17:19:50 -0800

    led

--- rov_pi/main.py
+++ rov_pi/main.py
@@ -1,14 +1,19 @@
 #script for rov pi
 #tested on my pc ONLY
 import socket,json
+import RPi.GPIO as GPIO  
+
+GPIO.setmode(GPIO.BOARD)             # choose BCM or ---BOARD(one we use)---  
+GPIO.setup(12, GPIO.OUT)
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect(('192.168.0.99', 6060))
 
-while True:
-    message = s.recv(2048)
-    controller_data = json.loads(str(message).strip("b'").strip("'"))
-    print("data resevied:")
-    print(f"x-axis: {controller_data['axis-0']}")
-    print(f"y-axis: {controller_data['axis-1']}")
-    print(f"a up: {controller_data['button-0']}")
+try:
+    while True:
+        message = s.recv(2048)
+        controller_data = json.loads(str(message).strip("b'").strip("'"))
+        GPIO.output(12,controller_data["button-0"])
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
